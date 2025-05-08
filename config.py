aee787224a362b92c1a3c4c2985368a70816100f
Jesus390 <jesusespinola90@gmail.com>
2025-05-08 00:31:04 -0300

    create config file

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-segura'
+    
+    # Base de datos SQLite ubicada en la carpeta 'instance'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'app.sqlite')
+    
+    # Desactiva una advertencia innecesaria
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
