c6fbb3eaae0fffd577da4fdebe20f7a319778b21
Jesus390 <jesusespinola90@gmail.com>
2025-05-08 04:33:15 -0300

    decorador para limitar el acceso segun el rol

---
+++ app/decorators.py
@@ -0,0 +1,16 @@
+from flask import abort
+from flask_login import current_user
+from functools import wraps
+
+# decorador para limitar el acceso según rol
+def role_required(*roles):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.is_authenticated:
+                abort(403)
+            if current_user.role not in roles:
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
