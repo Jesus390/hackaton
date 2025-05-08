0a7fb885b2728ff2c8fb2c0b5ce5f35cab4290a0
Jesus390 <jesusespinola90@gmail.com>
2025-05-08 03:12:59 -0300

    se elimino codigo redundante

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,27 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-login_manager = LoginManager()
+from config import Config
+from app.extensions import db, login_manager
+from app.models.user import User
 
 def create_app(config_class='config.Config'):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
+
+    # --- LoginManager ---
     login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'  # Ruta del login si el usuario no está autenticado
 
-    with app.app_context():
-        db.create_all()
-
-    # Importa los modelos aquí
-    from app.models import User, Professional
-
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+        
     # Registrar blueprints
     from app.main import main_bp
+    from app.auth import auth_bp
+    
     app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
 
     return app
