fd8d471eb0e4252155666517d7d648d97981857e
Jesus390 <jesusespinola90@gmail.com>
2025-05-08 04:56:17 -0300

    se agrego columnas nuevas

--- app/models/professional.py
+++ app/models/professional.py
@@ -1,5 +1,6 @@
 from app.extensions import db
 from flask_login import UserMixin
+from datetime import datetime
 
 class Professional(UserMixin, db.Model):
     __tablename__ = 'professionals'
@@ -9,3 +10,9 @@ class Professional(UserMixin, db.Model):
     profession = db.Column(db.String(100), nullable=False)
     bio = db.Column(db.Text)
     experience_years = db.Column(db.Integer)
+    location = db.Column(db.String(120), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    # Relación reversa (opcional, si lo necesitas desde User)
+    user = db.relationship('User', back_populates='professional_profile')
