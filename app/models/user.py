44272263961e0d510733afcf1061d70abda62b3f
Jesus390 <jesusespinola90@gmail.com>
2025-05-08 03:14:21 -0300

    implementacion de UserMixin

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,9 @@
-from app import db
+from app.extensions import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
