913ebe2e06f544125c66c97bf406553b2864149d
Jesus390 <jesusespinola90@gmail.com>
2025-05-08 00:58:40 -0300

    update nombre de la base de datos

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-segura'
     
     # Base de datos SQLite ubicada en la carpeta 'instance'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'app.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'serviya.db')
     
     # Desactiva una advertencia innecesaria
     SQLALCHEMY_TRACK_MODIFICATIONS = False
