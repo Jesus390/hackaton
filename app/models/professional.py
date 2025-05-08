430ccfb4c95d7f23c0363fc9ff5b4ce7729e1e69
Jesus390 <jesusespinola90@gmail.com>
2025-05-08 03:15:11 -0300

    implementacion de UserMixin

--- app/models/professional.py
+++ app/models/professional.py
@@ -1,8 +1,11 @@
-from app import db
+from app.extensions import db
+from flask_login import UserMixin
 
-class Professional(db.Model):
+class Professional(UserMixin, db.Model):
+    __tablename__ = 'professionals'
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     profession = db.Column(db.String(100), nullable=False)
     bio = db.Column(db.Text)
     experience_years = db.Column(db.Integer)
